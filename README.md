badba512da864466bcd7e1466b74718c6a917a41
pyliu <3634168+pyliu@users.noreply.github.com>
2021-03-15 22:21:05 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+<p align="center"><img src="https://i.imgur.com/PTqXTbx.png"></p>
+
+## Usage
+
+```bash
+# create an app
+$ nuxtron init my-app --example with-javascript
+
+# install dependencies
+$ cd my-app
+$ yarn (or `npm install`)
+
+# development mode
+$ yarn dev (or `npm run dev`)
+
+# production build
+$ yarn build (or `npm run build`)
+```
---
+++ renderer/nuxt.config.js
@@ -0,0 +1,112 @@
+export default {
+  head: {
+    title: '信差服務客戶端-桃園市地政局',
+    meta: [{
+      charset: 'utf-8'
+    },
+    {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1'
+    },
+    {
+      hid: 'description',
+      name: 'description',
+      content: '桃園市地政智慧控管系統'
+    }
+    ],
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }
+    ],
+    script: []
+  },
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend (config, ctx) {
+      config.target = 'electron-renderer';
+    },
+    babel: { compact: true }
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: {
+    color: '#5cb85c'
+  },
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+    // https://animate.style/
+    'animate.css/animate.css'
+  ],
+  purgeCSS: {
+    whitelistPatterns: [/svg.*/, /fa.*/]
+  },
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+    { src: '~/plugins/axios'}
+  ],
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    // '@nuxtjs/eslint-module'
+  ],
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    // https://go.nuxtjs.dev/bootstrap
+    'bootstrap-vue/nuxt',
+    '@nuxtjs/style-resources',
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    '@nuxtjs/proxy',
+    '@nuxtjs/localforage',
+    'nuxt-fontawesome'
+  ],
+  bootstrapVue: {
+    // Install the `IconsPlugin` plugin (in addition to `BootstrapVue` plugin)
+    icons: true,
+    config: {
+      // Custom config options here
+    }
+  },
+  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+  axios: {
+    baseURL: `${process.env.PROTOCOL}://${process.env.API_HOST}:${process.env.API_PORT}`,
+    proxy: true,
+    credentials: false,
+    https: process.env.PROTOCOL === 'https',
+    debug: false
+  },
+  proxy: {
+    '/api': {
+      target: `${process.env.PROTOCOL}://${process.env.API_HOST}:${process.env.API_PORT}`,
+      changeOrigin: true, 
+      // pathRewrite: {
+      //   '^/api': '',
+      // },
+    }
+  },
+  // should hold all env variables that are public as these will be exposed on the frontend.
+  // available using $config in both server and client.
+  publicRuntimeConfig: {
+    axios: {
+      // Default: baseURL; when the proxy option is true, it will become PREFIX instead of baseURL
+      browserBaseURL: process.env.BROWSER_BASE_URL
+    }
+  },
+  // should hold all env variables that are private and that should not be exposed on the frontend.
+  // only available on server using same $config
+  // privateRuntimeConfig always overrides publicRuntimeConfig on server-side. $config in server mode is { ...public, ...private } but for client mode only { ...public }
+  privateRuntimeConfig: {
+    axios: {
+      // Default: http://[HOST]:[PORT][PREFIX]
+      baseURL: process.env.BASE_URL
+    }
+  }
+}
