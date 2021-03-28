ec2c5ebffaa2d3a2501a518a6de6b917fb68e7fd
pyliu <3634168+pyliu@users.noreply.github.com>
2021-03-29 00:20:43 +0800

    add nuxt default layout

---
+++ renderer/layouts/default.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  .main-window(v-cloak)
+    b-row(align-v="center" align-h="between")
+      b-col aaa
+      b-col bbb
+    Nuxt
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+.main-window {
+  width: 640px;
+  height: 100vh;
+  border: 3px solid red;
+}
+</style>
