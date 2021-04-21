bd62c302eae46fec3e3350557579873e1b6fca67
pyliu <3634168+pyliu@users.noreply.github.com>
2021-04-21 22:11:53 +0800

    wording

--- renderer/pages/settings.vue
+++ renderer/pages/settings.vue
@@ -7,7 +7,7 @@
     b-input-group.my-2
       template(#prepend)
         b-icon.my-auto.mr-2(icon="person-badge" font-scale="2.25" variant="secondary")
-        span.my-auto 網域名稱
+        span.my-auto 網域姓名
       b-input.ml-2(v-model="nickname" placeholder="... 顯示名稱 ..." trim readonly)
     b-input-group.my-2
       template(#prepend)
