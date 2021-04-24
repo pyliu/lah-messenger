1ff908778ca44f41b94425a04a7419970338a59d
pyliu <3634168+pyliu@users.noreply.github.com>
2021-04-24 10:02:51 +0800

    scroll smoothly

--- renderer/components/message-board.vue
+++ renderer/components/message-board.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  div(:class="blockCss"): .msg(ref="box"): transition-group(name="list" mode="out-in")
+  div(:class="blockCss"): .msg(ref="box"): transition-group(name="listY" mode="out-in")
     message(v-for="(item, idx) in list" :raw="item" :prev="list[idx - 1]" :key="`msg-${idx}`" :ref="`msg-${idx}`")
 </template>
 
@@ -28,8 +28,13 @@ export default {
       // watch list to display the latest message
       // Vue VDOM workaround ... to display the last message
       this.$nextTick(() => {
-        if (this.$refs.box) {
-          this.$refs.box.scrollTop = this.$refs.box.scrollHeight
+        if (this.$refs.box && this.$refs[`msg-${this.list.length - 1}`]) {
+          const latest = this.$refs[`msg-${this.list.length - 1}`][0]
+          latest.$el.scrollIntoView && latest.$el.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"})
+          !latest.$el.scrollIntoView && (this.$refs.box.scrollTop = this.$refs.box.scrollHeight)
+          // 'bounce', 'flash', 'pulse', 'rubberBand', 'shakeX', 'shakeY', 'headShake', 'swing', 'tada', 'wobble'
+          (this.isPersonal || this.isAnnouncement) && this.attention(latest.$el, { name: 'tada', speed: 'slow' })
+          // this.$refs.box.scrollTop = this.$refs.box.scrollHeight
         }
       })
     }
