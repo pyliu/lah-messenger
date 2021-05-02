fc7683f341b6ea5fe0d3186ffe291bbd32328b63
pyliu <3634168+pyliu@users.noreply.github.com>
2021-05-02 18:47:30 +0800

    reduce attention effect in settings.vue

--- renderer/pages/settings.vue
+++ renderer/pages/settings.vue
@@ -106,7 +106,7 @@ export default {
       { value: 'supervisor', text: '主任秘書室' },
     ],
     effectVal: '',
-    effectOpts: [ '', 'bounce', 'flash', 'headShake', 'heartBeat', 'jello', 'pulse', 'rubberBand', 'shakeX', 'shakeY', 'swing', 'tada' ],
+    effectOpts: [ '', 'bounce', 'flash', 'headShake', 'pulse'],
   }),
   computed: {
     wsConnStr() {
