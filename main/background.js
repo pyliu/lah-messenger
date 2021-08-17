5b380f458683e2e062d6a65ab949dde9fe4332cb
pyliu <3634168+pyliu@users.noreply.github.com>
2021-08-17 22:39:38 +0800

    update guard code for tray

--- main/background.js
+++ main/background.js
@@ -1,5 +1,6 @@
-import { app, Tray, Menu } from 'electron'
+import { app, Tray, Menu, nativeImage } from 'electron'
 import serve from 'electron-serve'
+const fs = require('fs')
 const path = require('path')
 
 import {
@@ -11,6 +12,15 @@ import {
 const isProd = process.env.NODE_ENV === 'production'
 
 let mainWindow = null
+let tray = null
+
+const closeApp = function () {
+  tray && tray.destroy();
+  app.isQuiting = true
+  // send to renderer process
+  mainWindow && mainWindow.webContents.send('quit')
+  app.quit()
+}
 
 const gotTheLock = app.requestSingleInstanceLock()  // 鎖定視窗，並記錄回傳值
 if (!gotTheLock) {
@@ -33,87 +43,99 @@ if (!gotTheLock) {
   app.on('ready', () => {
     (async () => {
       await app.whenReady()
-      
-      // tray icon
-      const iconPath = isProd ? path.join(__dirname, 'taoyuan.ico') : path.join(__dirname, '..', 'resources', 'taoyuan.ico')
-      !isProd && console.log(`tray icon path`, iconPath)
-      const tray = new Tray(iconPath)
-      tray.setContextMenu(Menu.buildFromTemplate([{
-          label: '顯示視窗', click () { mainWindow.show() }
-        }, {
-          label: '隱藏視窗', click () { mainWindow.hide() }
-        }, {
-          label: '關閉程式(您將無法收到通知)',
-          click () {
-            app.isQuiting = true
-            app.quit()
-          }
+      try {
+        // tray icon
+        let iconPath = path.join(__dirname, 'taoyuan.ico')
+        if (!fs.existsSync(iconPath)) {
+          iconPath = path.join(__dirname, '..', 'resources', 'taoyuan.ico')
         }
-      ]))
-      tray.setIgnoreDoubleClickEvents(true)
-      tray.on('click', (event) => mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show())
-      tray.setToolTip('桃園地政-信差服務即時通')
 
-      mainWindow = createWindow('main', {
-        width: isProd ? 490 : 960,
-        height: 740,
-        show: false,  // use 'ready-to-show' event to show the window
-        useContentSize: true, // include window frame/menubar size
-        center: true,
-        resizable: false,
-        maximizable: false,
-        minimizable: true,
-        alwaysOnTop: false,
-        kiosk: false,
-        menuBarVisible: false,  // not working
-      })
-      // disable the menu bar since menuBarVisible flag does not work properly
-      mainWindow.setMenuBarVisibility(false)
-      
-      mainWindow.once('ready-to-show', function(e) {
-        // setTimeout(() => this.show(), 5000)
-        mainWindow.show()
-      })
-
-      mainWindow.on('focus', () => {
-        // when browser window focused, set always on top attr to false
-        mainWindow.setAlwaysOnTop(false)
-      })
-
-      if (isProd) {
-        await mainWindow.loadURL('app://./home')
-      } else {
-        const port = process.argv[2]
-        await mainWindow.loadURL(`http://localhost:${port}/home`)
-        mainWindow.webContents.openDevTools()
-      }
-
-      // open all a link with external browser
-      // https://github.com/electron/electron/issues/1344#issuecomment-359312676
-      const shell = require('electron').shell
-      mainWindow.webContents.on('will-navigate', (event, url) => {
-        if (!url.startsWith('http://localhost:8888')) {
-          event.preventDefault()
-          if (url.startsWith('http:') || url.startsWith('https:')) {
-            shell.openExternal(url)
+        !isProd && console.log(`tray icon path`, iconPath)
+        
+        const trayIcon = nativeImage.createFromPath(iconPath);
+        trayIcon.resize({ width: 16, height: 16 })
+        tray = new Tray(trayIcon)
+        
+        tray.setContextMenu(Menu.buildFromTemplate([{
+            label: '顯示視窗', click () { mainWindow.show() }
+          }, {
+            label: '隱藏視窗', click () { mainWindow.hide() }
+          }, {
+            label: '關閉程式(您將無法收到通知)',
+            click () {
+              app.isQuiting = true
+              app.quit()
+            }
           }
-        }
-      })
+        ]))
+        tray.setIgnoreDoubleClickEvents(true)
+        tray.on('click', (event) => mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show())
+        tray.setToolTip('桃園地政-信差服務即時通')
 
-      // minimize to tray
-      mainWindow.on('minimize', function(event) {
-        event.preventDefault()
-        mainWindow.hide()
-      })
-      
-      // close to tray
-      mainWindow.on('close', function (event) {
-        if (!app.isQuiting){
+        mainWindow = createWindow('main', {
+          width: isProd ? 490 : 960,
+          height: 740,
+          show: false,  // use 'ready-to-show' event to show the window
+          useContentSize: true, // include window frame/menubar size
+          center: true,
+          resizable: false,
+          maximizable: false,
+          minimizable: true,
+          alwaysOnTop: false,
+          kiosk: false,
+          menuBarVisible: false,  // not working
+        })
+        // disable the menu bar since menuBarVisible flag does not work properly
+        mainWindow.setMenuBarVisibility(false)
+        
+        mainWindow.once('ready-to-show', function(e) {
+          // setTimeout(() => this.show(), 5000)
+          mainWindow.show()
+        })
+
+        mainWindow.on('focus', () => {
+          // when browser window focused, set always on top attr to false
+          mainWindow.setAlwaysOnTop(false)
+        })
+
+        if (isProd) {
+          await mainWindow.loadURL('app://./home')
+        } else {
+          const port = process.argv[2]
+          await mainWindow.loadURL(`http://localhost:${port}/home`)
+          mainWindow.webContents.openDevTools()
+        }
+
+        // open all a link with external browser
+        // https://github.com/electron/electron/issues/1344#issuecomment-359312676
+        const shell = require('electron').shell
+        mainWindow.webContents.on('will-navigate', (event, url) => {
+          if (!url.startsWith('http://localhost:8888')) {
             event.preventDefault()
-            mainWindow.hide()
-        }
-        return false
-      })
+            if (url.startsWith('http:') || url.startsWith('https:')) {
+              shell.openExternal(url)
+            }
+          }
+        })
+
+        // minimize to tray
+        // mainWindow.on('minimize', function(event) {
+        //   event.preventDefault()
+        //   mainWindow.hide()
+        // })
+        
+        // close to tray
+        mainWindow.on('close', function (event) {
+          if (!app.isQuiting){
+              event.preventDefault()
+              mainWindow.hide()
+          }
+          return false
+        })
+      } catch (err) {
+        closeApp()
+        notify(err.message, '程式已關閉。')
+      }
     })()
   })
 }
@@ -125,16 +147,10 @@ if (isProd) {
   app.setPath('userData', `${app.getPath('userData')} (development)`)
 }
 
-app.on('window-all-closed', () => {
-  app.isQuiting = true
-  // send to renderer process
-  mainWindow.webContents.send('quit')
-  app.quit()
-})
+app.on('window-all-closed', closeApp)
 
 // ipc main process to handle renderer request 
 const { ipcMain } = require('electron')
-
 
 ipcMain.handle('quit', async (event, str) => {
   app.isQuiting = true
