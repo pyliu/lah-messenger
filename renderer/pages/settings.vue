1af25fc5cbf64dbb4f95d60a9d94e8f19a32b05e
pyliu <3634168+pyliu@users.noreply.github.com>
2021-09-03 10:08:16 +0800

    use validAdInfo

--- renderer/pages/settings.vue
+++ renderer/pages/settings.vue
@@ -26,7 +26,7 @@
           span.my-auto 網域密碼
         b-input.ml-2(:type="adPasswordType" v-model="adPassword" :placeholder="`${userid}的網域密碼`" trim)
         b-icon.my-auto.ml-2.eye(ref="eye" :icon="adPasswordIcon" font-scale="1.25" variant="secondary" @click="switchAdPasswordIcon")
-        b-button.ml-1(@click="queryAd" :disabled="empty(adPassword) || !$utils.isIPv4(adHost)" variant="outline-primary" title="透過AD取得顯示姓名") 查詢
+        b-button.ml-1(@click="queryAd" :disabled="!validAdInfo" :variant="validAdInfo ? 'primary' : 'danger'" title="透過AD取得顯示姓名") 查詢
       b-input-group.my-2
         template(#prepend)
           b-icon.my-auto.mr-2(icon="building" font-scale="2.25" variant="secondary")
@@ -123,7 +123,7 @@ export default {
     wsConnStr() {
       return `ws://${this.wsHost}:${this.wsPort}`
     },
-    validAdInfo() { return this.empty(this.adPassword) || !this.$utils.isIPv4(this.adHost) },
+    validAdInfo() { return !this.empty(this.adPassword) && this.$utils.isIPv4(this.adHost) },
     validAdHost() { return this.$utils.isIPv4(this.adHost) === false ? false : null },
     validHost() { return this.$utils.isIPv4(this.wsHost) === false ? false : null },
     validPort() {
