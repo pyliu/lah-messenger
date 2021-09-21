afa23f987fc24fa2c8047c340b1ec6468bca6a6b
pyliu <3634168+pyliu@users.noreply.github.com>
2021-09-21 13:09:57 +0800

    add send message support by user-card

--- renderer/components/user-card.vue
+++ renderer/components/user-card.vue
@@ -6,31 +6,51 @@
       b-icon(icon="exclamation-circle-fill" variant="danger")
       span.ml-1 找不到使用者資料
     b-card(v-else)
-      b-row
-        b-col(md="6")
-          b-card-title.
-            #[b-avatar.mt-n1(button variant="light" size="1.3em" :src="avatarSrc" @click="photoClick")]
-            {{ userData.name }}
+      b-card-title.
+        #[b-avatar.mt-n1(button variant="light" size="1.3em" :src="avatarSrc" @click="photoClick")]
+        {{ userData.name }}
 
-          b-card-sub-title.d-flex
-            span.mr-auto {{ userData.title }}
+      b-card-sub-title.d-flex
+        span.mr-auto {{ userData.title }}
 
-          b-card-text.d-flex.flex-column.small
+      b-card-text
+        .d-flex
+          .d-flex.flex-column.small.mr-auto
             div ＩＤ：{{ userData.id }}
             div(v-if="userData.ip") 電腦：{{ userData.ip }}
             div(v-if="userData.ext") 分機：{{ userData.ext }}
             div(v-if="userData.unit") 部門：{{ userData.unit }}
             div(v-if="userData.work") 工作：{{ userData.work }}
-
-        b-col.d.flex.flex-column(md="6")
-          b-link(@click="photoClick" title="放大顯示")
-            b-img-lazy.shadow(
-              :src="photoSrc"
-              :alt="userData.name"
-              fluid
-              thumbnail
-              rounded
+          .d-flex.small
+            b-textarea(
+              ref="userText"
+              v-model="message"
+              debounce="200"
+              placeholder="... 留言給他/她 ..."
+              size="sm"
+              @keyup.enter.exact="sendMessage"
+              no-resize
+              no-auto-shrink
+              autofocus
             )
+            b-button.mx-1(
+              @click="sendMessage"
+              size="sm"
+              :disabled="isMessageEmpty"
+              variant="outline-primary"
+            )
+              //- b-icon.mr-1(icon="cursor")
+              span 傳送
+
+      .d-flex.flex-column(md="6")
+        b-link(@click="photoClick" title="放大顯示")
+          b-img-lazy.shadow(
+            :src="photoSrc"
+            :alt="userData.name"
+            fluid
+            thumbnail
+            rounded
+          )
 </template>
 
 <script>
@@ -57,10 +77,11 @@ export default {
       onboard_date: '',
       authority: 0
     },
-    loading: true
+    loading: true,
+    message: ''
   }),
   computed: {
-    isDisabled () { return (this.userData.authority & this.$consts.AUTHORITY.DISABLED) === this.$consts.AUTHORITY.DISABLED },
+    isMessageEmpty () { return this.$utils.empty(this.message) },
     found () { return !this.$utils.empty(this.userData.id) },
     photoSrc () {
       return this.avatarSrc.replaceAll('_avatar', '')
@@ -96,6 +117,15 @@ export default {
     })
   },
   methods: {
+    sendMessage () {
+      if (this.websocket && !this.isMessageEmpty) {
+        this.websocket.send(this.packMessage(this.message, { channel: this.userData.id }))
+        // also send to own channel to simulate talking between eachothers
+        this.websocket.send(this.packMessage(`@${this.userData.name}<hr/>${this.message}`, { channel: this.userid }))
+      }
+      this.message = ''
+      this.$refs.userText && this.$refs.userText.focus()
+    },
     photoClick () {
       this.modal(this.$createElement('b-img', {
         attrs: {
