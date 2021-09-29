45f68522bb38dea49e59d04b31d201777e9a1fa3
pyliu <3634168+pyliu@users.noreply.github.com>
2021-09-29 22:22:46 +0800

    more

--- renderer/components/emoji-pickup.vue
+++ renderer/components/emoji-pickup.vue
@@ -34,6 +34,8 @@ export default {
         this.emojiLib.random().emoji,
         this.emojiLib.random().emoji,
         this.emojiLib.random().emoji,
+        this.emojiLib.random().emoji,
+        this.emojiLib.random().emoji,
         this.emojiLib.random().emoji
       ])
     }
