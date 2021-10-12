4c4b0b45398c6c450ec01099ddd764ddfcfb7391
pyliu <3634168+pyliu@users.noreply.github.com>
2021-10-12 16:37:32 +0800

    no icon for notification

--- main/helpers/notification.js
+++ main/helpers/notification.js
@@ -5,14 +5,14 @@ import debounce from 'lodash/debounce'
 const isProd = process.env.NODE_ENV === 'production'
 
 const notify = function (message, title, callback) {
-  const iconPath = path.join(__dirname, 'message.ico')
-  !isProd && console.log(`notification icon path`, iconPath)
+  // const iconPath = path.join(__dirname, 'message.ico')
+  // !isProd && console.log(`notification icon path`, iconPath)
   notifier.notify(
     {
       appID: '💬桃園即時通',
       title: title || message,
       message: title ? message : '',
-      icon: iconPath, // Absolute path (doesn't work on balloons)
+      // icon: iconPath, // Absolute path (doesn't work on balloons)
       sound: true, // Only Notification Center or Windows Toasters
       wait: true // Wait with callback, until user action is taken against notification, does not apply to Windows Toasters as they always wait or notify-send as it does not support the wait option
     },
