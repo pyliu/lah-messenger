16a36b5e4a9fc2a7f5efbd81082207c62ed8dbf3
pyliu <3634168+pyliu@users.noreply.github.com>
2022-06-09 13:01:23 +0800

    query online clients only when window is visible

--- renderer/plugins/global-vue-mixin.js
+++ renderer/plugins/global-vue-mixin.js
@@ -169,18 +169,20 @@ Vue.mixin({
       this.$store.commit("resetUnread", channel)
     },
     queryOnlineClients () {
-      const jsonString = JSON.stringify({
-        type: "command",
-        sender: this.userid,
-        date: this.date(),
-        time: this.time(),
-        message: JSON.stringify({
-          command: 'online',
-          channel: this.currentChannel
-        }),
-        channel: 'system'
-      })
-      this.websocket && this.websocket.send(jsonString)
+      if (this.windowVisible) {
+        const jsonString = JSON.stringify({
+          type: "command",
+          sender: this.userid,
+          date: this.date(),
+          time: this.time(),
+          message: JSON.stringify({
+            command: 'online',
+            channel: this.currentChannel
+          }),
+          channel: 'system'
+        })
+        this.websocket && this.websocket.send(jsonString)
+      }
     },
     clearReconnectTimer() {
       if (this.timer !== null) {
