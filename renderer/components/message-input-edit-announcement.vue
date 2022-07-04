c864361aff3252c0b6f4f4f8dc63fde5ffd4c828
pyliu <3634168+pyliu@users.noreply.github.com>
2022-07-04 10:36:04 +0800

    enable tray context menu

--- renderer/components/message-input-edit-announcement.vue
+++ renderer/components/message-input-edit-announcement.vue
@@ -247,12 +247,7 @@ export default {
           channel: this.channel,
           id: this.id,
           sender: this.userid,
-          payload: {
-            ...this.announcementJson,
-            title: this.title,
-            content: this.content,
-            priority: this.priority
-          }
+          payload: this.announcementJson
         },
         channel: 'system'
       }
