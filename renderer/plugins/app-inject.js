8363165f00e08fc96b631e32380ced571b1157db
pyliu <3634168+pyliu@users.noreply.github.com>
2025-09-17 13:49:56 +0800

    refactoring

--- renderer/plugins/app-inject.js
+++ renderer/plugins/app-inject.js
@@ -2,7 +2,6 @@ import createApiService from '~/plugins/api';
 import consts from '~/plugins/consts';
 import utils from '~/plugins/utils';
 
-import uploadAxios from 'axios';
 import qs from 'qs';
 
 /**
@@ -17,25 +16,17 @@ export default ({ $axios, store }, inject) => {
   inject('utils', utils)
   // 以下是 axios.js 的內容
   const cancelTokenSource = $axios.CancelToken.source();
+  inject('acts', cancelTokenSource) // e.g. this.$acts.cancel('axios request has been cancelled!') in Vue
   $axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
   $axios.onRequest(config => {
     if (config.data && config.headers[config.method]['Content-Type'] === 'application/x-www-form-urlencoded') {
       config.data = qs.stringify(config.data)
     }
     config.cancelToken = cancelTokenSource.token
-
     return config
   })
-  $axios.onResponse(response => {
-    
-  })
+  $axios.onResponse(response => { return response })
   $axios.onError(error => {
     utils.error(error)
-    // redirect('/error')
   })
-  inject('acts', cancelTokenSource) // e.g. this.$acts.cancel('axios request has been cancelled!') in Vue
-  // inject uploading file axios
-  // need to add 'Header set Access-Control-Allow-Origin "*"' to Apache site and turn on mod_header.so in httpd.conf
-  uploadAxios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-  inject('upload', uploadAxios)
 }
